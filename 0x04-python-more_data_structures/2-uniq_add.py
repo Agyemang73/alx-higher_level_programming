899bd2daed243c96b4054183ef84c6a348d368b9
Agyemang73 <owusukevin17@gmail.com>
2022-06-09 13:44:04 +0100

    correction

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def uniq_add((my_list=[]):
-        result = 0
-        for x in set(my_list):
-            result += x
-        return (result)    
+def uniq_add(my_list=[]):
+    sm = 0
+    list_uq = list(dict.fromkeys(my_list))
+    for i in range(len(list_uq)):
+        sm += list_uq[i]
+    return (sm)    
+
 
