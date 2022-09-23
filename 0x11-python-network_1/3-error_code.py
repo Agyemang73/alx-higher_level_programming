dc57953b11075a168a1a8859eaf3ba589c0d3955
owusu-alx <agyemangkevin57@gmail.com>
2022-09-23 22:10:33 +0100

    task completion

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+  GNU nano 6.4                      3-error_code.py
+
+
+
+
+
+
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL"""
+import urllib.request
+import urllib.parse
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            s = response.read()
+            print(s.decode('utf-8'))
+    except urllib.error.HTTPError as err:
+        print('Error code:', err.code)
